9977be16410dfa4056716aa680dcd2fdf1bddf8a
ishitajain139 <70798980+ishitajain139@users.noreply.github.com>
2023-08-30 11:19:07 +0530

    Update TestingController.java

--- src/main/java/com/sks/TestingController.java
+++ src/main/java/com/sks/TestingController.java
@@ -7,19 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 public class TestingController {
-	
-	@RequestMapping("/welcome")
-	public String welcome(@RequestParam("name") String name, @RequestParam("surname")String surname) {
-		return "Welcome " + name+ " " + surname + " to your first spring boot application";
-	}
-	
-	@RequestMapping("/home")
-	public String home() {
-		return "This is the home url";
-	}
-	
-	
-		
-	
-
+    
+    @RequestMapping("/welcome")
+    public String welcome(@RequestParam("name") String name, @RequestParam("surname")String surname) {
+        return "Welcome " + name+ " " + surname + " to your first spring boot application";
+    }
+    
+    @RequestMapping("/home")
+    public String home() {
+        return "This is the home url";
+    }
 }
