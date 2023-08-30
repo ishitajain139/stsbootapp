d89570674d52851170077a3f5246dd9d2c2bee9f
ishitajain139 <70798980+ishitajain139@users.noreply.github.com>
2023-08-30 11:19:50 +0530

    Update UserController.java

--- src/main/java/com/sks/UserController.java
+++ src/main/java/com/sks/UserController.java
@@ -7,30 +7,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
- 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-	@PostMapping("/info")
-	public User displayUserDetails(@RequestBody User user) {
-		user.setName(user.getName().toUpperCase());
-		user.setNationality(user.getNationality().toUpperCase());
-		return user;
-	}
-	
-	@GetMapping("/{name}/{age}")
-    public String displayUserName(@PathVariable String name,@PathVariable int age) {
-    return "Welcome " + name + " " + age;
-
+    
+    @PostMapping("/info")
+    public User displayUserDetails(@RequestBody User user) {
+        user.setName(user.getName().toUpperCase());
+        user.setNationality(user.getNationality().toUpperCase());
+        return user;
+    }
+    
+    @GetMapping("/{name}/{age}")
+    public String displayUserName(@PathVariable String name, @PathVariable int age) {
+        return "Welcome " + name + " " + age;
     }
 }
-
-
- 
-
-
-
-
-
-
- 
