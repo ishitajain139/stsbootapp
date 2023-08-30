16c64c6c0c2a618bf01937e6bb2a5ed14aa83667
ishitajain139 <70798980+ishitajain139@users.noreply.github.com>
2023-08-30 11:19:27 +0530

    Update User.java

--- src/main/java/com/sks/User.java
+++ src/main/java/com/sks/User.java
@@ -1,38 +1,44 @@
 package com.sks;
 
 public class User {
-	
-	private String name;
-	private int age;
-	private String nationality;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-	public String getNationality() {
-		return nationality;
-	}
-	public void setNationality(String nationality) {
-		this.nationality = nationality;
-	}
-	public User(String name, int age, String nationality) {
-		super();
-		this.name = name;
-		this.age = age;
-		this.nationality = nationality;
-	}
-	@Override
-	public String toString() {
-		return "User [name=" + name + ", age=" + age + ", nationality=" + nationality + "]";
-	}
-	
-	
+    
+    private String name;
+    private int age;
+    private String nationality;
+    
+    public User(String name, int age, String nationality) {
+        super();
+        this.name = name;
+        this.age = age;
+        this.nationality = nationality;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public String getNationality() {
+        return nationality;
+    }
+    
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+    
+    @Override
+    public String toString() {
+        return "User [name=" + name + ", age=" + age + ", nationality=" + nationality + "]";
+    }
 }
